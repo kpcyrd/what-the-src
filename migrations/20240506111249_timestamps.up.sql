5c95b7fad44d9b007d55bda2e19e7b9cec45afc8
kpcyrd <git@rxv.cc>
2024-05-06 20:02:03 +0200

    Fix database-type compatibility issue with timezones

--- migrations/20240506111249_timestamps.up.sql
+++ migrations/20240506111249_timestamps.up.sql
@@ -1,8 +1,8 @@
 -- Add timestamps to artifacts
 ALTER TABLE artifacts
 DROP COLUMN db_version,
-ADD COLUMN first_seen timestamp NOT NULL DEFAULT NOW(),
-ADD COLUMN last_imported timestamp;
+ADD COLUMN first_seen timestamptz NOT NULL DEFAULT NOW(),
+ADD COLUMN last_imported timestamptz;
 
 UPDATE artifacts
 SET last_imported = to_timestamp(0);
@@ -12,8 +12,8 @@ ALTER COLUMN last_imported SET NOT NULL;
 
 -- Add timestamps to refs
 ALTER TABLE refs
-ADD COLUMN first_seen timestamp NOT NULL DEFAULT NOW(),
-ADD COLUMN last_seen timestamp NOT NULL DEFAULT NOW();
+ADD COLUMN first_seen timestamptz NOT NULL DEFAULT NOW(),
+ADD COLUMN last_seen timestamptz NOT NULL DEFAULT NOW();
 
 ALTER TABLE refs
 ALTER COLUMN last_seen DROP DEFAULT;
